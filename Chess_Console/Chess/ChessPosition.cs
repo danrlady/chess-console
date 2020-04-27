ff4caa869e2fbd98fa889e9ff11c6e663b01a4ff
Danrley Tavares <danrleybruno@gmail.com>
2020-04-27 17:36:48 -0300

    Created ChessPosition class

---
+++ Chess_Console/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using GameBoard;
+
+namespace Chess
+{
+    class ChessPosition
+    {
+        public char Row { get; set; }
+        public int Column { get; set; }
+
+        public ChessPosition(char row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Row;
+        }
+    }
+}
